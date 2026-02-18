3f853f0aa2d6229e56363ba4e84907beac2db827
jan <jan.detleffsen@mpi-bn.mpg.de>
2026-02-18 21:34:13 +0100

    fixed bugs

--- src/peakqc/__init__.py
+++ src/peakqc/__init__.py
@@ -1,1 +1,14 @@
 """PEAKQC (peakqc)."""
+
+# import with prefix _ to hide them
+from ._version import __version__
+
+# define what is exported in this module
+__all__ = [
+    "__version__"
+]
+
+
+def __dir__():
+    """Return the defined submodules."""
+    return __all__
